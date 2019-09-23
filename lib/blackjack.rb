f9ebd6e32a508357693eb596c23304757c6902a1
Will Drougas <wdrougas@gmail.com>
2019-09-23 22:50:19 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,14 +29,21 @@ def initial_round
 end
 
 def hit?(card_total)
+ valid_inputs = ["h", "s"]
+
   prompt_user
   user_input = get_user_input
+
+  until valid_inputs.include?(user_input)
+    invalid_command
+    prompt_user
+    user_input = get_user_input
+  end
 
   if user_input == "h"
     card_total += deal_card
   end
   card_total
-end
 
 def invalid_command
   puts "Please enter a valid command"
